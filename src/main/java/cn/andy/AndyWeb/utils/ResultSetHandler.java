bb19677e6ce00d7dc8168a81b98b9163ae84feeb
Andy <13003782538@163.com>
2021-05-26 23:28:04 +0800

    en

---
+++ src/main/java/cn/andy/AndyWeb/utils/ResultSetHandler.java
@@ -0,0 +1,7 @@
+package cn.andy.AndyWeb.utils;
+
+import java.sql.ResultSet;
+
+public interface ResultSetHandler {
+    Object hanlder(ResultSet resultSet);
+}
--- src/main/java/cn/andy/AndyWeb/utils/dbutil.java
+++ src/main/java/cn/andy/AndyWeb/utils/dbutil.java
@@ -1,5 +1,6 @@
 package cn.andy.AndyWeb.utils;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -19,14 +20,13 @@ public class dbutil {
      *
      * */
 
-    private static String  driver = null;
-    private static String  url = null;
-    private static String  username = null;
+    private static String driver = null;
+    private static String url = null;
+    private static String username = null;
     private static String password = null;
 
     static {
         try {
-
             //获取配置文件的读入流
             InputStream inputStream = dbutil.class.getClassLoader().getResourceAsStream("db.properties");
 
@@ -43,17 +43,17 @@ public class dbutil {
             Class.forName(driver);
 
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url,username,password);
+        return DriverManager.getConnection(url, username, password);
     }
+
+
     public static void release(Connection connection, Statement statement, ResultSet resultSet) {
 
         if (resultSet != null) {
@@ -78,4 +78,56 @@ public class dbutil {
             }
         }
     }
+
+
+    public static void update(String sql, Object[] objects) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            //根据传递进来的参数，设置SQL占位符的值
+            for (int i = 0; i < objects.length; i++) {
+                preparedStatement.setObject(i + 1, objects[i]);
+            }
+            //执⾏SQL语句
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /*
+ 1:对于查询语句来说，我们不知道对结果集进⾏什么操作【常⽤的就是把数据封装成⼀个Bean
+对象，封装成⼀个List集合】
+ 2:我们可以定义⼀个接⼝，让调⽤者把接⼝的实现类传递进来
+ 3:这样接⼝调⽤的⽅法就是调⽤者传递进来实现类的⽅法。【策略模式】
+ */
+    //这个⽅法的返回值是任意类型的，所以定义为Object。
+    public static Object query(String sql, Object[] objects, ResultSetHandler
+            rsh) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            //根据传递进来的参数，设置SQL占位符的值
+            if (objects != null) {
+                for (int i = 0; i < objects.length; i++) {
+                    preparedStatement.setObject(i + 1, objects[i]);
+                }
+            }
+            resultSet = preparedStatement.executeQuery();
+            //调⽤调⽤者传递进来实现类的⽅法，对结果集进⾏操作
+            return rsh.hanlder(resultSet);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
 }
